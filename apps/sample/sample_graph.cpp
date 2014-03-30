2374e57f4a4fa8c9a8bcb8bfb97e942282a04d83
Tej Chajed <tchajed+github@gmail.com>
2014-03-29 20:59:33 -0500

    Print number of iterations required

--- apps/sample/sample_graph.cpp
+++ apps/sample/sample_graph.cpp
@@ -148,6 +148,7 @@ int main(int argc, char** argv) {
     }
   }
   
+  dc.cout() << "took " << iteration << " iterations" << std::endl;
   dc.cout() << "sampled " << num_sampled << " vertices" << std::endl;
   dc.cout() << "actual/target " << (double) num_sampled / (double) target_sample << std::endl;
   
